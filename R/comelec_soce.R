16fdfb21bfe0de83025bbbaf0451bf4910590712
SJP2503 <piggott.simon@yahoo.com>
2025-08-28 17:20:02 +0800

    Update comelec_soce.R
    Add COMELEC SOCE, Courts, and DBM normalizers

--- R/comelec_soce.R
+++ R/comelec_soce.R
@@ -9,16 +9,17 @@ library(lubridate)
 
 dir.create("data/comelec", recursive = TRUE, showWarnings = FALSE)
 
+# Normalizes a raw SOCE CSV you drop at data/input/comelec_soce_raw.csv
 normalize_soce <- function(path = "data/input/comelec_soce_raw.csv") {
   if (!file.exists(path)) return(tibble())
   raw <- suppressMessages(read_csv(path, show_col_types = FALSE)) |> clean_names()
 
   nm <- names(raw)
   contr <- nm[which(nm %in% c("contributor","donor","contributor_name"))[1]]
-  recip <- nm[which(nm %in% c("recipient","candidate","candidate_party"))[1]]
-  amt   <- nm[which(nm %in% c("amount","amount_donated","value"))[1]]
+  recip <- nm[which(nm %in% c("recipient","candidate","candidate_party","beneficiary"))[1]]
+  amt   <- nm[which(nm %in% c("amount","amount_donated","value","amount_php"))[1]]
   year  <- nm[which(nm %in% c("year","filed_year","election_year"))[1]]
-  ref   <- nm[which(nm %in% c("ref","reference","doc","source"))[1]]
+  ref   <- nm[which(nm %in% c("ref","reference","doc","source","url"))[1]]
 
   out <- tibble(
     contributor = if (!is.na(contr)) raw[[contr]] else NA_character_,
