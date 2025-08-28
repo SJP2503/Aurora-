153079a10f6e85f2cd96ef8a2358c1e358310b04
SJP2503 <piggott.simon@yahoo.com>
2025-08-28 17:23:01 +0800

    Update dbm_releases.R
    Add COMELEC SOCE, Courts, and DBM normalizers

--- R/dbm_releases.R
+++ R/dbm_releases.R
@@ -10,16 +10,17 @@ library(digest)
 
 dir.create("data/dbm", recursive = TRUE, showWarnings = FALSE)
 
+# Normalizes a raw DBM releases CSV you drop at data/input/dbm_releases_raw.csv
 normalize_dbm <- function(path = "data/input/dbm_releases_raw.csv") {
   if (!file.exists(path)) return(tibble())
   raw <- suppressMessages(read_csv(path, show_col_types = FALSE)) |> clean_names()
 
   nm <- names(raw)
-  agency <- nm[which(nm %in% c("agency","department","entity"))[1]]
+  agency <- nm[which(nm %in% c("agency","department","entity","procuring_entity"))[1]]
   amount <- nm[which(nm %in% c("amount","release_amount","obligation","allotment"))[1]]
   dt     <- nm[which(nm %in% c("date","release_date","posting_date"))[1]]
   rtype  <- nm[which(nm %in% c("release_type","type","uacs_type"))[1]]
-  ref    <- nm[which(nm %in% c("ref","reference","doc","url"))[1]]
+  ref    <- nm[which(nm %in% c("ref","reference","doc","url","source"))[1]]
 
   out <- tibble(
     agency = if (!is.na(agency)) raw[[agency]] else NA_character_,
